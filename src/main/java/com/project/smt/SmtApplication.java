b740a35b1ec9a76d9f9ca5aa0aad871d8079363c
Dmitrii Dergachov <dergachovda@gmail.com>
2020-10-12 23:24:55 +0300

    init project

---
+++ src/main/java/com/project/smt/SmtApplication.java
@@ -0,0 +1,13 @@
+package com.project.smt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmtApplication.class, args);
+    }
+
+}
